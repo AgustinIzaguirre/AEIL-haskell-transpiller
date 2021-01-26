c2435b6bc11500917be6234fa37c71d645e3b49f
Agustin Izaguirre <Agustinizag1@gmail.com>
2021-01-26 19:38:42 -0300

    Correct identation

--- app/Main.hs
+++ app/Main.hs
@@ -15,7 +15,7 @@ main :: IO ()
 main = do
     args <- getArgs
     case args of 
-      [file] -> do
-        x <- readFile file
-        putStr x
-      _ -> putStrLn "Wrong number of arguments"
+        [file] -> do
+            x <- readFile file
+            putStr x
+        _ -> putStrLn "Wrong number of arguments, should provide one file."
