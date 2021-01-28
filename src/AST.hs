35e0ae3f1bdb8010fa650fdf982acc8ef8509053
Agustin Izaguirre <Agustinizag1@gmail.com>
2021-01-27 21:15:17 -0300

    Update AST and Lexer

--- src/AST.hs
+++ src/AST.hs
@@ -4,26 +4,32 @@ type Name = String
 
 data Program = Root Function
                 | Multiple Function Program
+                deriving (Show)
 
-data Function = Name [Name] Block
+data Function = Name [Name] Block 
+                deriving(Show)
 
 data Block = Empty
                 | Actions Statement Block
+                deriving(Show)
                 -- | ConditionalBlock Block
                 -- | LoopBlock Block
                 -- TODO
 
 data Statement = Asign Name ValueExp
                 | Return ValueExp
+                deriving(Show)
                 -- | PrintFunc
-                --  TODO
+                -- TODO
 
 data ValueExp = BoolExp
                 | ArithmeticExp
                 | StringExp
                 | APPLY FuncCall
+                deriving(Show)
 
 data FuncCall = Call Name [ValueExp]
+                deriving(Show)
 
 data BoolExp = True
             | False
@@ -33,6 +39,7 @@ data BoolExp = True
             | Not BoolExp
             | RelationalBinaryArithmetic RelationalBinaryOperator ArithmeticExp ArithmeticExp
             | RelationalBinaryString RelationalBinaryOperator StringExp StringExp
+            deriving(Show)
 
 data RelationalBinaryOperator = Equals
                                 | NotEquals
@@ -40,25 +47,31 @@ data RelationalBinaryOperator = Equals
                                 | LessOrEqual
                                 | Greater
                                 | GreaterOrEqual
+                                deriving(Show)
 data BoolBinaryOperators = And
                         | Or
+                        deriving(Show)
 
 data ArithmeticExp = Number Integer
                | NumVar Name
                | NumFunc FuncCall
                | Negate ArithmeticExp
                | ArithmeticBinaryOperation ArithmeticBinaryOperator ArithmeticExp ArithmeticExp
+               deriving(Show)
 
 data ArithmeticBinaryOperator = Add
                                 | Minus
                                 | Multiply
                                 | Divide
                                 | Modulo
+                                deriving(Show)
 
 
 data StringExp = StringValue String
                 | StringVar Name
                 | StringFunc FuncCall
                 | StringBinaryOperation StringOperators StringExp StringExp
+                deriving(Show)
 
 data StringOperators = Concat
+                        deriving(Show)
