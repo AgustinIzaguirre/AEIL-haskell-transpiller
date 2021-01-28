a9d2b9cb2e92f9a7a1dae802eb5035f1fdbec40f
Agustin Izaguirre <Agustinizag1@gmail.com>
2021-01-28 00:54:12 -0300

    Basic parser that only parses if implemented

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,19 +1,19 @@
 module Parser where
 
-import Text.Parsec ()
+import Text.Parsec (sepBy1, (<|>))
 import Text.Parsec.String (Parser)
 
 import qualified Text.Parsec.Expr as Expr
 
 import Lexer
 import AST
-    ( ArithmeticBinaryOperator(Modulo, Minus, Add, Multiply, Divide),
+    (Statement(Block), ValueExp(BoolValue),  Statement(If),  ArithmeticBinaryOperator(Modulo, Minus, Add, Multiply, Divide),
       ArithmeticExp(ArithmeticBinaryOperation, Negate),
       BoolBinaryOperators(Or, And),
-      BoolExp(BoolBinaryOperations, Not),
-      ValueExp(ArithmeticExp),
+      BoolExp(FalseValue, TrueValue, BoolBinaryOperations, Not),
       Program )
 import qualified Control.Monad.Identity as Data.Functor.Identity
+import qualified Text.Parsec as Text.Parsec.Prim
 
 arithmeticOperators =
             [
@@ -32,3 +32,42 @@ booleanOperators =
         [ Expr.Infix (reservedOperators "&&" >> return (BoolBinaryOperations And )) Expr.AssocLeft,
           Expr.Infix (reservedOperators "||" >> return (BoolBinaryOperations Or )) Expr.AssocLeft ]
     ]
+
+mainParser :: Parser Statement
+mainParser = whiteSpace >> block
+
+block :: Parser Statement
+block = parenthesis block
+        <|> statementList
+
+statementList :: Parser Statement
+statementList = do 
+    statements <- sepBy1 statement semiColon
+    return (if length statements == 1 then head statements else Block statements)
+
+statement :: Parser Statement 
+statement = ifStatement
+            -- <|> returnStatement
+
+ifStatement :: Parser Statement
+ifStatement = do
+    reserved "if"
+    condition <- parenthesis booleanExpression
+    ifBlock <- braces statement
+    return (If condition ifBlock)
+
+booleanExpression :: Parser BoolExp
+booleanExpression = Expr.buildExpressionParser booleanOperators boolean
+
+boolean :: Parser BoolExp 
+boolean = parenthesis booleanExpression
+            <|> (reserved "true" >> return TrueValue)
+            <|> (reserved "false" >> return FalseValue)
+
+-- returnStatement :: Parser Statement
+-- returnStatement = do
+--     reserved "return" >> return valueExp
+
+-- valueExp :: Parser ValueExp
+-- valueExp = parenthesis valueExp
+--             <|> return (BoolValue boolean)
