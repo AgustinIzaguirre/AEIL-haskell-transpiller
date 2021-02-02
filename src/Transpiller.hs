36afba8773d5493562137e180ff667a3372903a6
Agustin Izaguirre <Agustinizag1@gmail.com>
2021-02-02 17:28:57 -0300

    Basic transpilation of functions implemented

--- src/Transpiller.hs
+++ src/Transpiller.hs
@@ -2,6 +2,8 @@ module Transpiller where
 
 import AST
 
+type FunctionData = (String,([String], Function))
+
 -- Probabbly make a function that returns [(FuncName, ([Parameters, FuncAST))] from Program
 -- hasMain $ map fst pairTest where pairTest is the array of [(FuncName, ([Parameters, FuncAST))]
 -- parameters to check if quantity matches on every call, funcName to check for main and to check function exists
@@ -9,16 +11,38 @@ import AST
 hasMain :: [String] -> Bool
 hasMain funcNames = (== 1) . length $ filter (== "main") funcNames
 
+hasMainFunction :: [FunctionData] -> Bool
+hasMainFunction functionsData
+        | hasMain $ map fst functionsData = maybe False (null . fst . snd) (getFunction functionsData "main")
+        | otherwise = False
+
 -- getFunction :: [(String, (Int, Function))] -> Name -> Maybe (String, (Int, Function))
-getFunction :: [(String, (Int, String))] -> String -> Maybe (String, (Int, String))
+getFunction :: [FunctionData] -> String -> Maybe FunctionData
 getFunction functions functionName
     | length functionInfo == 1 = Just (head functionInfo)
     | otherwise = Nothing -- Not declared or redeclared
     where functionInfo = filter ((== functionName) . fst) functions
 
--- isValidFuncCall :: (String, (Int, Function)) -> [(String, (Int, Function))] -> Bool
-isValidFuncCall :: (String, (Int, String)) -> [(String, (Int, String))] -> Bool
+-- isValidFuncCall :: (String, ([String], Function)) -> [(String, ([String], Function))] -> Bool
+isValidFuncCall :: FunctionData -> [FunctionData] -> Bool
 isValidFuncCall call functions = case function of
                             Nothing -> False
-                            Just f -> (fst . snd) call == (fst . snd) f 
+                            Just f -> (length. fst . snd) call == (length . fst . snd) f 
                         where function = getFunction functions (fst call)
+
+-- TODO replace Maybe with Either
+transpileProgram :: Program -> Maybe String
+transpileProgram program 
+                | hasMainFunction programFunctions = Just (concat (fmap transpileFunction programFunctions))
+                | otherwise = Nothing
+                where programFunctions = getProgramFunctions program
+
+getProgramFunctions :: Program -> [FunctionData]
+getProgramFunctions (Root function) = [getFunctionData function]
+getProgramFunctions (Multiple function program) = getFunctionData function : getProgramFunctions program
+
+getFunctionData :: Function -> FunctionData
+getFunctionData (Func name parameters block) = (name, (parameters, Func name parameters block))
+
+transpileFunction :: FunctionData -> String
+transpileFunction funcData = "def " ++ fst funcData ++ ":\n\t" ++ "pass\n"
