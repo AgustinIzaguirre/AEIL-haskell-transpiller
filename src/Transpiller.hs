fe0fbe32f77b28a0a5f70475e0da746669952d29
AgustinIzaguirre <Agustinizag1@gmail.com>
2021-02-05 19:25:35 -0300

    Basic transpiling (#7)
    * Basic transpilation of functions implemented
    * Add simulated block transpilation
    * Add parameters to functions
    * Add tests for hasMainFunction

--- src/Transpiller.hs
+++ src/Transpiller.hs
@@ -1,24 +1,27 @@
 module Transpiller where
 
 import AST
+import Lib
+import Data.List
 
 -- Probabbly make a function that returns [(FuncName, ([Parameters, FuncAST))] from Program
 -- hasMain $ map fst pairTest where pairTest is the array of [(FuncName, ([Parameters, FuncAST))]
 -- parameters to check if quantity matches on every call, funcName to check for main and to check function exists
 
-hasMain :: [String] -> Bool
-hasMain funcNames = (== 1) . length $ filter (== "main") funcNames
+-- TODO replace Maybe with Either
+transpileProgram :: Program -> Maybe String
+transpileProgram program 
+                | hasMainFunction programFunctions = Just (concat (fmap transpileFunction programFunctions) ++ "\nmain()")
+                | otherwise = Nothing
+                where programFunctions = getProgramFunctions program
 
--- getFunction :: [(String, (Int, Function))] -> Name -> Maybe (String, (Int, Function))
-getFunction :: [(String, (Int, String))] -> String -> Maybe (String, (Int, String))
-getFunction functions functionName
-    | length functionInfo == 1 = Just (head functionInfo)
-    | otherwise = Nothing -- Not declared or redeclared
-    where functionInfo = filter ((== functionName) . fst) functions
+transpileFunction :: FunctionData -> String
+transpileFunction funcData = "def " ++ fst funcData ++ "(" ++ 
+                                transpileFuncParameters ((fst . snd) funcData) ++ "):\n" ++ 
+                                transpileBlock ((getFunctionBlock . snd . snd) funcData) 1 ++ "\n"
 
--- isValidFuncCall :: (String, (Int, Function)) -> [(String, (Int, Function))] -> Bool
-isValidFuncCall :: (String, (Int, String)) -> [(String, (Int, String))] -> Bool
-isValidFuncCall call functions = case function of
-                            Nothing -> False
-                            Just f -> (fst . snd) call == (fst . snd) f 
-                        where function = getFunction functions (fst call)
+transpileBlock :: Block -> Int -> String 
+transpileBlock block level = identForLevel 1 ++ "pass\n"
+
+transpileFuncParameters :: [String] -> String
+transpileFuncParameters = intercalate ", " 
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,35 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+import Test.HUnit
+import Lib
+import AST
+
+emptyFunction :: String -> [String] -> Function
+emptyFunction name params = Func name params Empty
+
+-- data
+functionsWithoutMain :: [FunctionData]
+functionsWithoutMain = [("func1",([], emptyFunction "func1" [])), ("func2",([], emptyFunction "func2" []))]
+
+functionsWithMainWithParams :: [FunctionData]
+functionsWithMainWithParams = [("func1",([], emptyFunction "func1" [])), ("func2",([], emptyFunction "func2" [])), ("main",(["param1"], emptyFunction "main" ["param1"]))]
+
+functionsWithValidMain :: [FunctionData]
+functionsWithValidMain = [("func1",([], emptyFunction "func1" [])), ("func2",([], emptyFunction "func2" [])), ("main",([], emptyFunction "main" []))]
+
+-- tests
+hasMainFunction1 :: Test
+hasMainFunction1 = TestCase (assertEqual "for hasMainFunction with no main function" False (hasMainFunction functionsWithoutMain))
+
+hasMainFunction2 :: Test
+hasMainFunction2 = TestCase (assertEqual "for hasMainFunction with main function with params" False (hasMainFunction functionsWithMainWithParams))
+
+hasMainFunction3 :: Test
+hasMainFunction3 = TestCase (assertEqual "for hasMainFunction with valid main" True (hasMainFunction functionsWithValidMain))
+
+tests :: Test
+tests = TestList [TestLabel "hasMainFunction with no main function" hasMainFunction1,
+                  TestLabel "hasMainFunction with main function with params" hasMainFunction2,
+                  TestLabel "hasMainFunction with main function with valid main" hasMainFunction3]
+
+
+main :: IO Counts
+main = runTestTT tests
